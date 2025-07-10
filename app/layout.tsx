6f778ff572d8e381efba9f0f5565b64de37f7ab0
Sabal-Subedi <subedisabal00@gmail.com>
2025-07-09 22:15:51 -0600

    Bug fix: removed unused imports

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import { cn } from '@/lib/utils'; 
 import ProviderWrapper from "@/redux/providerWrapper";
 
 const geistSans = Geist({
